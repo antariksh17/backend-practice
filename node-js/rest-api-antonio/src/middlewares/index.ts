b02faa33d3e99308816e82b2739c2dfdead327e3
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2023-05-23 20:29:39 +0530

    middlewares

---
+++ node-js/rest-api-antonio/src/middlewares/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import {get,merge} from 'lodash';
+import { getUserbySessionToken } from 'db/users';
+
+
+export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction)=> {
+    try{
+        const sessionToken = req.cookies['ANT-AUTH'];
+
+        if(!sessionToken){
+            return res.sendStatus(403);
+
+        }
+
+        const existingUser = await getUserbySessionToken(sessionToken);
+
+        if(!existingUser){
+            return res.sendStatus(403);
+        }
+
+        merge(req, {identity: existingUser}); 
+    }catch(error){
+        console.log(error)
+
+        return res.sendStatus(400);
+    }
+}
