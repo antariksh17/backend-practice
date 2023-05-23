fa12d75566af64ac0a25fa1813fab2aac1c06440
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2023-05-23 19:14:53 +0530

    added login controller

--- node-js/rest-api-antonio/src/router/authentication.ts
+++ node-js/rest-api-antonio/src/router/authentication.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 
 
-import {register} from '../controllers/authentication';
+import {login, register} from '../controllers/authentication';
 console.log("inside auth")
 export default (router: express.Router) => {
     router.post('auth/register', register);
+
+    router.post('auth/login', login);
+
 }
