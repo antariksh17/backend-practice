77e160ac563856b4298c7337559ebedeb84df0d5
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2023-05-27 10:50:00 +0530

    initial routing setup

---
+++ node-js/rest-api-tdt/src/routes.ts
@@ -0,0 +1,9 @@
+import {Express, Request, Response} from 'express';
+
+
+function routes(app: Express){
+
+    app.get('/healthcheck', (req:Request,res:Response)=> res.status(200))
+}
+
+export default routes;
